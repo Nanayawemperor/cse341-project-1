780dc4c77225b81e36f8641d72974a8292a64a3c
EmperorNanayaw <nanayawemperor095@gmail.com>
2025-05-14 22:47:27 +0000

    installed swagger

--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,10 @@
 const express = require('express');
 const router = express.Router();
 
+router.use('/', require('./swagger'));
+
 router.get('/', (req, res) => {
+  //#swagger.tags=['Hello from the routes!']
   res.send('Hello from the routes!');
 });
 
